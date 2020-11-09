e33631c3979fdea74a2fd4e33f3f2dffbd400089
Ondra Chaloupka <ochaloup@redhat.com>
2020-11-09 17:15:03 +0100

    quartz scheduled without jdbc just simple with stdout

--- src/main/java/org/acme/quickstart/TaskBean.java
+++ src/main/java/org/acme/quickstart/TaskBean.java
@@ -9,11 +9,8 @@ import io.quarkus.scheduler.Scheduled;
 @ApplicationScoped
 public class TaskBean {
 
-    @Transactional
     @Scheduled(every = "10s")
     void schedule() {
-        Task task = new Task();
-        task.persist();
         System.out.println("HEY");
     }
 }
